37fdcf678f833854779b7052e3cd9744ed16374e
OlehTitov <oleg.titov81@gmail.com>
2020-08-03 19:15:29 +0300

    initial commit

---
+++ SmartKitty/SmartKitty/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  SmartKitty
+//
+//  Created by Oleh Titov on 03.08.2020.
+//  Copyright © 2020 Oleh Titov. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
