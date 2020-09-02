ebe4dca9d021102d52e564c7acb1f27f4fbceeca
OlehTitov <oleg.titov81@gmail.com>
2020-09-02 08:56:04 +0300

    feat: Add InstructionsVC

--- SmartKitty/SmartKitty/Controller/InstructionsVC.swift
+++ SmartKitty/SmartKitty/Controller/InstructionsVC.swift
@@ -1,37 +1,25 @@
 //
-//  WelcomeVC.swift
+//  InstructionsVC.swift
 //  SmartKitty
 //
-//  Created by Oleh Titov on 31.08.2020.
+//  Created by Oleh Titov on 02.09.2020.
 //  Copyright © 2020 Oleh Titov. All rights reserved.
 //
 
 import Foundation
 import UIKit
 
-class WelcomeVC: UIViewController {
+class InstructionVC: UIViewController {
     
-    //MARK: - PROPERTIES
     let signupURL = "https://smartcat.ai"
     
-    //MARK: - OUTLETS
-    @IBOutlet weak var signInButton: PrimaryButton!
-    @IBOutlet weak var imageHeight: NSLayoutConstraint!
-    
-    //MARK: - VIEW DID LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
-        setImageHeight()
         setGradientBackground()
-    }
-   
-    func setImageHeight() {
-        let viewHeigh = view.frame.size.height
-        imageHeight.constant = viewHeigh * 0.3
     }
     
     func setGradientBackground() {
-        let colorTop =  UIColor.coldPurple.cgColor
+        let colorTop =  UIColor.antiqueFuchsia.cgColor
         let colorBottom = UIColor.cloudBurst.cgColor
         let gradientLayer = CAGradientLayer()
         gradientLayer.colors = [colorTop, colorBottom]
@@ -43,16 +31,17 @@ class WelcomeVC: UIViewController {
         goToNextVC()
     }
     
+    
     @IBAction func createAccountTapped(_ sender: Any) {
         let url = URL(string: signupURL)
         UIApplication.shared.open(url!, options: [ : ], completionHandler: nil)
     }
     
     func goToNextVC() {
-        let instructionVC = self.storyboard?.instantiateViewController(identifier: "InstructionVC") as! InstructionVC
-        instructionVC.modalPresentationStyle = .fullScreen
-        instructionVC.modalTransitionStyle = .crossDissolve
-        present(instructionVC, animated: true, completion: nil)
+        let loginVC = self.storyboard?.instantiateViewController(identifier: "LoginVC") as! LoginVC
+        loginVC.modalPresentationStyle = .fullScreen
+        loginVC.modalTransitionStyle = .crossDissolve
+        present(loginVC, animated: true, completion: nil)
     }
     
 }
